b018c81e93f25b14107b6af18fb15e17b936c0e4
Aleksa0308 <aleksas0308@gmail.com>
2023-10-31 19:17:43 +0100

    checkpoint continue work home

---
+++ src/main/java/framework/annotations/Autowired.java
@@ -0,0 +1,9 @@
+package framework.annotations;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Autowired {
+    boolean verbose() default false;
+}
